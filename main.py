5a486e2ea540a614ee3de652218d9ed61ba9421b
Himesh kumar <2022pietcshimesh071@poornima.org>
2025-11-17 13:37:22 +0530

    model completed and updated

--- main.py
+++ main.py
@@ -42,4 +42,32 @@ def main():
     if not os.path.exists('output'):
         os.makedirs('output')
 
-   
+    preprocessedOimg = preprocess(oimg)
+    cv2.imwrite('output/preprocessedOimg.jpg', preprocessedOimg)
+
+    clusteredImg = kMeans_cluster(preprocessedOimg)
+    cv2.imwrite('output/clusteredImg.jpg', clusteredImg)
+
+    edgedImg = edgeDetection(clusteredImg)
+    cv2.imwrite('output/edgedImg.jpg', edgedImg)
+
+    boundRect, contours, contours_poly, img = getBoundingBox(edgedImg)
+    pdraw = drawCnt(boundRect[1], contours, contours_poly, img)
+    cv2.imwrite('output/pdraw.jpg', pdraw)
+
+    croppedImg, pcropedImg = cropOrig(boundRect[1], clusteredImg)
+    cv2.imwrite('output/croppedImg.jpg', croppedImg)
+    
+    newImg = overlayImage(croppedImg, pcropedImg)
+    cv2.imwrite('output/newImg.jpg', newImg)
+
+    fedged = edgeDetection(newImg)
+    fboundRect, fcnt, fcntpoly, fimg = getBoundingBox(fedged)
+    fdraw = drawCnt(fboundRect[2], fcnt, fcntpoly, fimg)
+    cv2.imwrite('output/fdraw.jpg', fdraw)
+
+    print("feet size (cm): ", calcFeetSize(pcropedImg, fboundRect)/10)
+
+
+if __name__ == '__main__':
+    main()
